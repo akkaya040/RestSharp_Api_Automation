7352dc8d24eb411b3bc11425f16fa7b54dbcfadc
Kurtulus M. Akkaya <akkaya040@gmail.com>
2022-09-06 13:33:42 +0300

    Initial commit: First Project

---
+++ RestSharp Api Automation/UnitTest1.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace RestSharp_Api_Automation;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
