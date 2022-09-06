a3ce144bdd2d173939e6a154c1b79709eccdcba9
Kurtulus M. Akkaya <akkaya040@gmail.com>
2022-09-06 14:29:38 +0300

    Api Class Added into the project

---
+++ RestSharp Api Automation/Drivers/Api.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using RestSharp;
+
+namespace RestSharp_Api_Automation.Drivers;
+
+public class Api
+{
+    public RestClient? restClient { get; set; }
+    public RestRequest? restRequest { get; set; }
+    public RestResponse? restResponse { get; set; }
+    public string? baseUrl { get; set; }
+    public string? endPoint { get; set; }
+    public string? responseString { get; set; }
+    
+    
+    public RestResponse GetRequest(string endP, Dictionary<string, string>? headers = null)
+    {
+        restClient = new RestClient(baseUrl ?? throw new InvalidOperationException());
+        restRequest = new RestRequest(endP, Method.Get);
+        restRequest.AddHeader("Content-Type", "application/json");
+        restRequest.AddHeader("Accept", "application/json");
+
+        if (headers != null)
+        {
+            foreach (var header in headers)
+            {
+                restRequest.AddHeader(header.Key, header.Value);
+            }
+        }
+
+        restResponse = restClient.ExecuteGetAsync(restRequest).GetAwaiter().GetResult();
+        responseString = restResponse.Content;
+
+        return restResponse;
+    }
+    
+    public RestResponse PostRequest(string endP, string? body = null,
+        Dictionary<string, string>? headers = null)
+    {
+        restClient = new RestClient(baseUrl ?? throw new InvalidOperationException());
+        restRequest = new RestRequest(endP, Method.Post);
+
+        restRequest.AddHeader("Content-Type", "application/json;charset=utf-8");
+        restRequest.AddHeader("Accept", "*/*");
+        restRequest.AddHeader("Connection", "keep-alive");
+        restRequest.AddHeader("Accept-Encoding", "gzip, deflate, br");
+
+        if (headers != null)
+        {
+            foreach (var header in headers)
+            {
+                restRequest.AddHeader(header.Key, header.Value);
+            }
+        }
+        
+        if (body != null)
+        {
+            restRequest.AddParameter("application/json; charset=utf-8", JsonConvert.SerializeObject(body), ParameterType.RequestBody);
+        }
+
+        restResponse = restClient.ExecutePostAsync(restRequest).GetAwaiter().GetResult();
+        responseString = restResponse.Content;
+        return restResponse;
+    }
+    
+    public RestResponse PostRequest(string endP, Dictionary<string, string>? body = null,
+        Dictionary<string, string>? headers = null)
+    {
+        restClient = new RestClient(baseUrl ?? throw new InvalidOperationException());
+        restRequest = new RestRequest(endP, Method.Post);
+
+        restRequest.AddHeader("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8");
+        restRequest.AddHeader("Accept", "application/json, text/plain, */*");
+
+        if (headers != null)
+        {
+            foreach (var header in headers)
+            {
+                restRequest.AddHeader(header.Key, header.Value);
+            }
+        }
+
+        if (body != null)
+        {
+            foreach (var param in body)
+            {
+                restRequest.AddParameter(param.Key, param.Value, ParameterType.GetOrPost);
+            }
+        }
+
+        restResponse = restClient.ExecutePostAsync(restRequest).GetAwaiter().GetResult();
+        responseString = restResponse.Content;
+        return restResponse;
+    }
+    
+}
--- RestSharp Api Automation/UnitTest1.cs
+++ RestSharp Api Automation/UnitTest1.cs
@@ -1,17 +1,32 @@
 using NUnit.Framework;
+using RestSharp_Api_Automation.Drivers;
 
 namespace RestSharp_Api_Automation;
 
 public class Tests
 {
+    
+    private Api _api = new();
+    
     [SetUp]
     public void Setup()
     {
+        _api.baseUrl = "https://petstore.swagger.io/v2";
     }
 
     [Test]
     public void Test1()
     {
+        _api.endPoint = "/pet/findByStatus?status=available";
+        _api.GetRequest(_api.endPoint);
+        string responseString = _api.responseString;
+        
         Assert.Pass();
+    }
+
+    [TearDown]
+    public void Stop()
+    {
+        
     }
 }
